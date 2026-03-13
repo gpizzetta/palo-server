c6b35f55eb9e1812ba24bf8f61cb0fb6c5e3e33c
Gilles Pizzetta <gilles@berdoz-vision.ch>
2026-03-13 18:40:24 +0100

    fix for debian 13

--- Library/Logger/Logger.h
+++ Library/Logger/Logger.h
@@ -1,4 +1,4 @@
-/* 
+/*
  *
  * Copyright (C) 2006-2014 Jedox AG
  *
@@ -23,7 +23,7 @@
  *
  * \author Frank Celler, triagens GmbH, Cologne, Germany
  * \author Achim Brandt, triagens GmbH, Cologne, Germany
- * 
+ *
  *
  */
 
@@ -34,262 +34,292 @@
 
 #include <iostream>
 #include <sstream>
+#include <vector>
+#include <cstddef>
 
 extern "C" {
-#include <time.h>
-#ifdef PALO_HMMM_FINER_LOGGING
-#include <sys/time.h>
-#endif
+	#include <time.h>
+	#ifdef PALO_HMMM_FINER_LOGGING
+	#include <sys/time.h>
+	#endif
 }
 
 #include "Thread/WriteLocker.h"
 
 namespace palo {
 
-////////////////////////////////////////////////////////////////////////////////
-/// @brief logger
-///
-/// This class provides various static members which can be used as logging
-/// streams. Output to the logging stream is appended by using the operator <<,
-/// as soon as a line is completed endl should be used to flush the stream.
-/// Each line of output is prefixed by some informational data.
-////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////
+	/// @brief logger
+	///
+	/// This class provides various static members which can be used as logging
+	/// streams. Output to the logging stream is appended by using the operator <<,
+	/// as soon as a line is completed endl should be used to flush the stream.
+	/// Each line of output is prefixed by some informational data.
+	////////////////////////////////////////////////////////////////////////////////
 
-class SERVER_CLASS Logger {
-public:
-	class LoggerStream {
+	class SERVER_CLASS Logger {
 	public:
-		LoggerStream(ostream& output) :
+		class LoggerStream {
+		public:
+			LoggerStream(ostream& output) :
 			on(true), output(&output), duplicateToStdout(false) {
+			}
+
+		public:
+			template<typename T>
+			LoggerStream& operator<<(const std::vector<T>& value) {
+				WriteLocker w(&m);
+				if (on) {
+					(*output) << "[";
+					for (std::size_t i = 0; i < value.size(); ++i) {
+						if (i > 0) {
+							(*output) << ", ";
+						}
+						(*output) << value[i];
+					}
+					(*output) << "]";
+
+					if (duplicateToStdout) {
+						cout << "[";
+						for (std::size_t i = 0; i < value.size(); ++i) {
+							if (i > 0) {
+								cout << ", ";
+							}
+							cout << value[i];
+						}
+						cout << "]";
+					}
+					output->flush();
+				}
+				return *this;
+			}
+
+			template<typename T>
+			LoggerStream& operator<<(const T& value) {
+				WriteLocker w(&m);
+				if (on) {
+					(*output) << value;
+					if (duplicateToStdout) {
+						cout << value;
+					}
+					output->flush();
+				}
+				return *this;
+			}
+
+			#if defined(_MSC_VER)
+			template<>
+			LoggerStream& operator<< (const socket_t& value) {
+				WriteLocker w(&m);
+				if (on) {
+					(*output) << (unsigned int) value;
+					if (duplicateToStdout) {
+						cout << (unsigned int) value;
+					}
+				}
+				return *this;
+			}
+
+			/*
+			 L ogg*erStream& operator<< (const size_t& value) {
+			 WriteLocker w(&m);
+			 if (on) {
+				 (*output) << (unsigned int) value;
 		}
+		return *this;
+		}
+		*/
+			#endif
+
+			LoggerStream& operator<<(ostream& (*fptr)(ostream&)) {
+				WriteLocker w(&m);
+				if (on) {
+					fptr((*output));
+					if (duplicateToStdout) {
+						fptr(cout);
+					}
+					output->flush();
+				}
+				return *this;
+			}
+
+			void activate() {
+				WriteLocker w(&m);
+				on = true;
+			}
+
+			void deactivate() {
+				WriteLocker w(&m);
+				on = false;
+			}
+
+			void setStream(ostream& os, bool duplicateToStdout) {
+				WriteLocker w(&m);
+				output = &os;
+				this->duplicateToStdout = duplicateToStdout;
+			}
+
+		private:
+			Mutex m;
+			bool on;
+			ostream* output;
+			bool duplicateToStdout;
+		};
+
+	private:
+		class LoggerStreamHeader {
+		public:
+			LoggerStreamHeader(const string& prefix, ostream&);
+
+		public:
+			template<typename T>
+			LoggerStream& operator<<(const T& value) {
+
+				if (on) {
+					#ifndef PALO_HMMM_FINER_LOGGING
+					time_t tt = time(0);
+					struct tm* t = localtime(&tt);
+					int max = 32;
+					char* s = new char[max];
+					strftime(s, max, "%Y-%m-%d %H:%M:%S ", t);
+					stream << s;
+					/*if (duplicateToStdout) {
+					 c *out << s;
+				}*/
+					delete[] s;
+					#endif
+					#ifdef PALO_HMMM_FINER_LOGGING
+					struct timeval tv;
+					gettimeofday(&tv, 0);
+					stream << tv.tv_sec << "." << tv.tv_usec << " " << clock() << " ";
+					#endif
+
+					stream << prefix << ": ";
+					stream << value;
+					/*if (duplicateToStdout) {
+					 c *out << prefix << ": " << value;
+				}*/
+					logger.linesCount++;
+				}
+
+				return stream;
+			}
+
+			void activate() {
+				on = true;
+				stream.activate();
+			}
+
+			void deactivate() {
+				on = false;
+				stream.deactivate();
+			}
+
+			bool isActive() {
+				return on;
+			}
+
+			void setStream(ostream& os, bool duplicateToStdout) {
+				this->duplicateToStdout = duplicateToStdout;
+				stream.setStream(os, duplicateToStdout);
+			}
+
+		private:
+			bool on;
+			const string prefix;
+			LoggerStream stream;
+			bool duplicateToStdout;
+		};
 
 	public:
-		template<typename T>
-		LoggerStream& operator<<(const T& value) {
-			WriteLocker w(&m);
-			if (on) {
-				(*output) << value;
-				if (duplicateToStdout) {
-					cout << value;
-				}
-				output->flush();
-			}
-			return *this;
+
+		////////////////////////////////////////////////////////////////////////////////
+		/// @brief changes log level
+		////////////////////////////////////////////////////////////////////////////////
+
+		static void setLogLevel(const string& level);
+
+		////////////////////////////////////////////////////////////////////////////////
+		/// @brief changes the log file
+		///
+		/// "-" means stdout, "+" means stderr
+		////////////////////////////////////////////////////////////////////////////////
+
+		static void setLogFile(const string& file, bool duplicateToStdout);
+
+		////////////////////////////////////////////////////////////////////////////////
+		/// @brief retrieves the log file
+		///
+		/// stdout or stderr returns empty string
+		////////////////////////////////////////////////////////////////////////////////
+
+		static string getLogFile() {return logger.fileName;}
+
+		////////////////////////////////////////////////////////////////////////////////
+		/// @brief retrieves the size of the file when opened for append
+		////////////////////////////////////////////////////////////////////////////////
+		static streampos getZeroPos() {return logger.zeroPosition;}
+
+		////////////////////////////////////////////////////////////////////////////////
+		/// @brief retrieves the count of lines added to log in current session
+		////////////////////////////////////////////////////////////////////////////////
+		static uint64_t getLinesCount() {return logger.linesCount;}
+
+		////////////////////////////////////////////////////////////////////////////////
+		/// @brief logger for error messages
+		////////////////////////////////////////////////////////////////////////////////
+
+		static LoggerStreamHeader error;
+
+		////////////////////////////////////////////////////////////////////////////////
+		/// @brief logger for warning messages
+		////////////////////////////////////////////////////////////////////////////////
+
+		static LoggerStreamHeader warning;
+
+		////////////////////////////////////////////////////////////////////////////////
+		/// @brief logger for info messages
+		////////////////////////////////////////////////////////////////////////////////
+
+		static LoggerStreamHeader info;
+
+		////////////////////////////////////////////////////////////////////////////////
+		/// @brief logger for debug messages
+		////////////////////////////////////////////////////////////////////////////////
+
+		static LoggerStreamHeader debug;
+
+		////////////////////////////////////////////////////////////////////////////////
+		/// @brief logger for trace messages
+		////////////////////////////////////////////////////////////////////////////////
+
+		static LoggerStreamHeader trace;
+
+		////////////////////////////////////////////////////////////////////////////////
+		/// @brief is enabled for debug messages
+		////////////////////////////////////////////////////////////////////////////////
+
+		static bool isDebug() {
+			return debug.isActive();
 		}
 
-#if defined(_MSC_VER)
-		template<>
-		LoggerStream& operator<< (const socket_t& value) {
-			WriteLocker w(&m);
-			if (on) {
-				(*output) << (unsigned int) value;
-				if (duplicateToStdout) {
-					cout << (unsigned int) value;
-				}
-			}
-			return *this;
-		}
+		////////////////////////////////////////////////////////////////////////////////
+		/// @brief is enabled for trace messages
+		////////////////////////////////////////////////////////////////////////////////
 
-/*
-		LoggerStream& operator<< (const size_t& value) {
-			WriteLocker w(&m);
-			if (on) {
-				(*output) << (unsigned int) value;
-			}
-			return *this;
-		}
-*/
-#endif
-
-		LoggerStream& operator<<(ostream& (*fptr)(ostream&)) {
-			WriteLocker w(&m);
-			if (on) {
-				fptr((*output));
-				if (duplicateToStdout) {
-					fptr(cout);
-				}
-				output->flush();
-			}
-			return *this;
-		}
-
-		void activate() {
-			WriteLocker w(&m);
-			on = true;
-		}
-
-		void deactivate() {
-			WriteLocker w(&m);
-			on = false;
-		}
-
-		void setStream(ostream& os, bool duplicateToStdout) {
-			WriteLocker w(&m);
-			output = &os;
-			this->duplicateToStdout = duplicateToStdout;
+		static bool isTrace() {
+			return trace.isActive();
 		}
 
 	private:
-		Mutex m;
-		bool on;
-		ostream* output;
-		bool duplicateToStdout;
-	};
-
-private:
-	class LoggerStreamHeader {
-	public:
-		LoggerStreamHeader(const string& prefix, ostream&);
-
-	public:
-		template<typename T>
-		LoggerStream& operator<<(const T& value) {
-
-			if (on) {
-#ifndef PALO_HMMM_FINER_LOGGING
-				time_t tt = time(0);
-				struct tm* t = localtime(&tt);
-				int max = 32;
-				char* s = new char[max];
-				strftime(s, max, "%Y-%m-%d %H:%M:%S ", t);
-				stream << s;
-				/*if (duplicateToStdout) {
-					cout << s;
-				}*/
-				delete[] s;
-#endif
-#ifdef PALO_HMMM_FINER_LOGGING
-				struct timeval tv;
-				gettimeofday(&tv, 0);
-				stream << tv.tv_sec << "." << tv.tv_usec << " " << clock() << " ";
-#endif
-
-				stream << prefix << ": ";
-				stream << value;
-				/*if (duplicateToStdout) {
-					cout << prefix << ": " << value;
-				}*/
-				logger.linesCount++;
-			}
-
-			return stream;
-		}
-
-		void activate() {
-			on = true;
-			stream.activate();
-		}
-
-		void deactivate() {
-			on = false;
-			stream.deactivate();
-		}
-
-		bool isActive() {
-			return on;
-		}
-
-		void setStream(ostream& os, bool duplicateToStdout) {
-			this->duplicateToStdout = duplicateToStdout;
-			stream.setStream(os, duplicateToStdout);
-		}
+		Logger();
 
 	private:
-		bool on;
-		const string prefix;
-		LoggerStream stream;
-		bool duplicateToStdout;
+		static Logger logger;
+		string fileName;
+		streampos zeroPosition;
+		uint64_t linesCount;
 	};
-
-public:
-
-	////////////////////////////////////////////////////////////////////////////////
-	/// @brief changes log level
-	////////////////////////////////////////////////////////////////////////////////
-
-	static void setLogLevel(const string& level);
-
-	////////////////////////////////////////////////////////////////////////////////
-	/// @brief changes the log file
-	///
-	/// "-" means stdout, "+" means stderr
-	////////////////////////////////////////////////////////////////////////////////
-
-	static void setLogFile(const string& file, bool duplicateToStdout);
-
-	////////////////////////////////////////////////////////////////////////////////
-	/// @brief retrieves the log file
-	///
-	/// stdout or stderr returns empty string
-	////////////////////////////////////////////////////////////////////////////////
-
-	static string getLogFile() {return logger.fileName;}
-
-	////////////////////////////////////////////////////////////////////////////////
-	/// @brief retrieves the size of the file when opened for append
-	////////////////////////////////////////////////////////////////////////////////
-	static streampos getZeroPos() {return logger.zeroPosition;}
-
-	////////////////////////////////////////////////////////////////////////////////
-	/// @brief retrieves the count of lines added to log in current session
-	////////////////////////////////////////////////////////////////////////////////
-	static uint64_t getLinesCount() {return logger.linesCount;}
-
-	////////////////////////////////////////////////////////////////////////////////
-	/// @brief logger for error messages
-	////////////////////////////////////////////////////////////////////////////////
-
-	static LoggerStreamHeader error;
-
-	////////////////////////////////////////////////////////////////////////////////
-	/// @brief logger for warning messages
-	////////////////////////////////////////////////////////////////////////////////
-
-	static LoggerStreamHeader warning;
-
-	////////////////////////////////////////////////////////////////////////////////
-	/// @brief logger for info messages
-	////////////////////////////////////////////////////////////////////////////////
-
-	static LoggerStreamHeader info;
-
-	////////////////////////////////////////////////////////////////////////////////
-	/// @brief logger for debug messages
-	////////////////////////////////////////////////////////////////////////////////
-
-	static LoggerStreamHeader debug;
-
-	////////////////////////////////////////////////////////////////////////////////
-	/// @brief logger for trace messages
-	////////////////////////////////////////////////////////////////////////////////
-
-	static LoggerStreamHeader trace;
-
-	////////////////////////////////////////////////////////////////////////////////
-	/// @brief is enabled for debug messages
-	////////////////////////////////////////////////////////////////////////////////
-
-	static bool isDebug() {
-		return debug.isActive();
-	}
-
-	////////////////////////////////////////////////////////////////////////////////
-	/// @brief is enabled for trace messages
-	////////////////////////////////////////////////////////////////////////////////
-
-	static bool isTrace() {
-		return trace.isActive();
-	}
-
-private:
-	Logger();
-
-private:
-	static Logger logger;
-	string fileName;
-	streampos zeroPosition;
-	uint64_t linesCount;
-};
 
 }
 
--- Modules/CMake/FindPerftools.cmake
+++ Modules/CMake/FindPerftools.cmake
@@ -1,4 +1,4 @@
-# - Try to find Google perftools include dirs and libraries 
+# - Try to find Google perftools include dirs and libraries
 #
 # Usage of this module as follows:
 #
@@ -20,27 +20,41 @@
 # Variables defined by this module:
 #
 #  Perftools_FOUND           System has Google perftools, this means the
-#                            include dir and all the libraries were found.  
-#  Perftools_INCLUDE_DIRS    Google perftools include directories. 
+#                            include dir and all the libraries were found.
+#  Perftools_INCLUDE_DIRS    Google perftools include directories.
 #  Perftools_LIBRARIES       Link these to use the Google perftools libraries.
 #
 #  Perftools_TCMALLOC_LIBRARY        Path to the tcmalloc library.
 #  Perftools_STACKTRACE_LIBRARY      Path to the stacktrace library.
 #  Perftools_PROFILER_LIBRARY        Path to the profiler library.
 
+# Reset helper search dirs
+set(Perftools_ADDITIONAL_INCLUDE_SEARCH_DIRS)
+set(Perftools_ADDITIONAL_LIBRARY_SEARCH_DIRS)
+
 if (PERFTOOLS_ROOT)
-    set(Perftools_ADDITIONAL_INCLUDE_SEARCH_DIRS ${PERFTOOLS_ROOT}/include)
-    set(Perftools_ADDITIONAL_LIBRARY_SEARCH_DIRS ${PERFTOOLS_ROOT}/lib)
+    list(APPEND Perftools_ADDITIONAL_INCLUDE_SEARCH_DIRS
+        ${PERFTOOLS_ROOT}/include
+    )
+    list(APPEND Perftools_ADDITIONAL_LIBRARY_SEARCH_DIRS
+        ${PERFTOOLS_ROOT}/lib
+        ${PERFTOOLS_ROOT}/lib64
+        ${PERFTOOLS_ROOT}/lib/x86_64-linux-gnu
+        ${PERFTOOLS_ROOT}/lib/aarch64-linux-gnu
+    )
 endif ()
 
 if (PERFTOOLS_INCLUDEDIR)
-    set(Perftools_ADDITIONAL_INCLUDE_SEARCH_DIRS ${PERFTOOLS_ROOT}/include)
+    list(APPEND Perftools_ADDITIONAL_INCLUDE_SEARCH_DIRS
+        ${PERFTOOLS_INCLUDEDIR}
+    )
 endif ()
 
 if (PERFTOOLS_LIBRARYDIR)
-    set(Perftools_ADDITIONAL_LIBRARY_SEARCH_DIRS ${PERFTOOLS_ROOT}/lib)
+    list(APPEND Perftools_ADDITIONAL_LIBRARY_SEARCH_DIRS
+        ${PERFTOOLS_LIBRARYDIR}
+    )
 endif ()
-
 
 if (Perftools_LIBRARIES AND Perftools_INCLUDE_DIRS)
     # In cache already.
@@ -49,79 +63,99 @@ else ()
     find_path(Perftools_INCLUDE_DIRS
         NAMES
             google/heap-profiler.h
+            gperftools/heap-profiler.h
         PATHS
             ${Perftools_ADDITIONAL_INCLUDE_SEARCH_DIRS}
             /usr/local/include
             /opt/local/include
             /sw/include
             /usr/include
-        )
+    )
 
     # tcmalloc
-    set(tcmalloc_names ${tcmalloc_names} tcmalloc)
+    set(tcmalloc_names
+        tcmalloc
+        tcmalloc_minimal
+    )
     find_library(perftools_tcmalloc_library
-        NAMES 
+        NAMES
             ${tcmalloc_names}
-        PATHS 
+        PATHS
             ${Perftools_ADDITIONAL_LIBRARY_SEARCH_DIRS}
             /usr/local/lib
+            /usr/local/lib64
             /opt/local/lib
             /sw/lib
             /usr/lib
-      )
+            /usr/lib64
+            /usr/lib/x86_64-linux-gnu
+            /usr/lib/aarch64-linux-gnu
+    )
 
     if (perftools_tcmalloc_library AND Perftools_INCLUDE_DIRS)
         set(Perftools_TCMALLOC_LIBRARY ${perftools_tcmalloc_library})
-        set(Perftools_LIBRARIES 
+        set(Perftools_LIBRARIES
             ${Perftools_LIBRARIES} ${perftools_tcmalloc_library})
         set(Perftools_FOUND true)
     else ()
         set(Perftools_FOUND false)
     endif ()
 
-
     # stacktrace
-    set(stacktrace_names ${stacktrace_names} stacktrace)
+    set(stacktrace_names
+        stacktrace
+    )
     find_library(perftools_stacktrace_library
-        NAMES 
+        NAMES
             ${stacktrace_names}
-        PATHS 
+        PATHS
             ${Perftools_ADDITIONAL_LIBRARY_SEARCH_DIRS}
             /usr/local/lib
+            /usr/local/lib64
             /opt/local/lib
             /sw/lib
             /usr/lib
-      )
+            /usr/lib64
+            /usr/lib/x86_64-linux-gnu
+            /usr/lib/aarch64-linux-gnu
+    )
 
     if (perftools_stacktrace_library AND Perftools_INCLUDE_DIRS)
         set(Perftools_STACKTRACE_LIBRARY ${perftools_stacktrace_library})
-        set(Perftools_LIBRARIES 
+        set(Perftools_LIBRARIES
             ${Perftools_LIBRARIES} ${perftools_stacktrace_library})
     endif ()
 
-
     # profiler
-    set(profiler_names ${profiler_names} profiler)
+    set(profiler_names
+        profiler
+    )
     find_library(perftools_profiler_library
-        NAMES 
+        NAMES
             ${profiler_names}
-        PATHS 
+        PATHS
             ${Perftools_ADDITIONAL_LIBRARY_SEARCH_DIRS}
             /usr/local/lib
+            /usr/local/lib64
             /opt/local/lib
             /sw/lib
             /usr/lib
-      )
+            /usr/lib64
+            /usr/lib/x86_64-linux-gnu
+            /usr/lib/aarch64-linux-gnu
+    )
 
     if (perftools_profiler_library AND Perftools_INCLUDE_DIRS)
         set(Perftools_PROFILER_LIBRARY ${perftools_profiler_library})
-        set(Perftools_LIBRARIES 
+        set(Perftools_LIBRARIES
             ${Perftools_LIBRARIES} ${perftools_profiler_library})
     endif ()
 
     if (Perftools_FOUND)
         if (NOT Perftools_FIND_QUIETLY)
             message(STATUS "Found Google perftools")
+            message(STATUS "  Perftools includes: ${Perftools_INCLUDE_DIRS}")
+            message(STATUS "  Perftools libraries: ${Perftools_LIBRARIES}")
         endif ()
     else ()
         if (Perftools_FIND_REQUIRED)
@@ -130,10 +164,10 @@ else ()
     endif ()
 
     mark_as_advanced(
-      Perftools_INCLUDE_DIRS
-      Perftools_LIBRARIES
-      Perftools_TCMALLOC_LIBRARY
-      Perftools_STACKTRACE_LIBRARY
-      Perftools_PROFILER_LIBRARY
+        Perftools_INCLUDE_DIRS
+        Perftools_LIBRARIES
+        Perftools_TCMALLOC_LIBRARY
+        Perftools_STACKTRACE_LIBRARY
+        Perftools_PROFILER_LIBRARY
     )
-endif()
+endif ()
